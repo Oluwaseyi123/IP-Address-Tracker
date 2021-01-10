d8a83a51c600a7ca1088e16e6c6524ebdf51b1e8
Oluwaseyi Aimudo <oluwaseyiaimudo@Oluwaseyis-MacBook-Pro.local>
2021-01-10 20:22:21 +0100

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-let myMap = L.map('mapid').setView([51.505, -0.09], 13)
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoib2x1d2FzZXlpMSIsImEiOiJja2V2eXhyOW4xdTBsMnduMGoyYTcyaTRwIn0.ERCgBbMvExyRSDh991amwA'
-}).addTo(myMap);
+let latitude;
+let longitude;
+let myMap = L.map('mapid');
+function showMap(){
+    myMap.setView([latitude, longitude], 13)
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+          attribution: false,
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+         zoomOffset: -1,
+         accessToken: config.MY_API_TOKEN
+    }).addTo(myMap);
 
-const ctaBtn = document.querySelector('.cta-btn')
-ctaBtn.addEventListener('click', checkIpAddress)
+    L.marker([latitude, longitude]).addTo(myMap);  
+ }
+
 class GetIpAddress{
-    api_key = 'at_i7kFwYf8ZCGWRa3RJWWF0ThX6R9pZ';
-    async getIp(){
+      api_key = config.SECRET_API_KEY;
+      async getIp(){
         let ipAddress = document.querySelector('.ip-address').value
         const response = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_i7kFwYf8ZCGWRa3RJWWF0ThX6R9pZ&ipAddress=${ipAddress}`, 
         {
@@ -20,27 +25,31 @@ class GetIpAddress{
         })
         const responseJSON = await response.json()
         return responseJSON;
-    }
-
+    }  
 }
-
-function checkIpAddress(){
-    const getIpAddress = new GetIpAddress()
-
+function displayInfo(res){
     const ip = document.querySelector('.ip')
     const location = document.querySelector('.location')
     const timezone = document.querySelector('.timezone')
     const isp = document.querySelector('.isp')
-    getIpAddress.getIp().then((res) => {
-        console.log(res)
-       
-        ip.textContent = res.ip
-        location.textContent = `${res.location.region}, ${res.location.country}`
-        timezone.textContent = res.location.timezone
-        isp.textContent = res.isp
+    ip.textContent = res.ip
+    location.textContent = `${res.location.region}, ${res.location.country}`
+    timezone.textContent = res.location.timezone
+    isp.textContent = res.isp
+}
+function checkIpAddress(){
+    const getIpAddress = new GetIpAddress()
+    getIpAddress.getIp().then( res => {
+        longitude = res.location.lng
+        latitude = res.location.lat
+        displayInfo(res)
+        showMap()
         
-
     }).catch((err) => {
         console.log(err)
     })
 }
+checkIpAddress()
+
+const ctaBtn = document.querySelector('.cta-btn').addEventListener('click', checkIpAddress)
+
---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    MY_API_TOKEN : 'pk.eyJ1Ijoib2x1d2FzZXlpMSIsImEiOiJja2V2eXhyOW4xdTBsMnduMGoyYTcyaTRwIn0.ERCgBbMvExyRSDh991amwA',
+    SECRET_API_KEY : 'at_i7kFwYf8ZCGWRa3RJWWF0ThX6R9pZ',
+    //api_key = 'at_i7kFwYf8ZCGWRa3RJWWF0ThX6R9pZ',
+    //accessToken: 'pk.eyJ1Ijoib2x1d2FzZXlpMSIsImEiOiJja2V2eXhyOW4xdTBsMnduMGoyYTcyaTRwIn0.ERCgBbMvExyRSDh991amwA'
+  }
